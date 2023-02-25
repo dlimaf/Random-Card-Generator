596f0d1628be576abd8be5c7fc099e4a969279cd
dlimaf <dlimaf1995@gmail.com>
2023-02-25 15:42:32 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let valor = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let valorRandom = Math.floor(Math.random() * valor.length);
+  let resultado = valor[valorRandom];
+  document.querySelector("p").innerHTML = resultado;
+
+  let pica = ["♦", "♥", "♠", "♣"];
+  let picaRandom = Math.floor(Math.random() * pica.length);
+  let resultado2 = pica[picaRandom];
+  let arriba = document.querySelector("#pica");
+  if (resultado2 == "♦" || resultado2 == "♥") {
+    arriba.style.color = "red";
+  }
+  arriba.innerHTML = resultado2;
+  let abajo = document.querySelector("#pica2");
+  if (resultado2 == "♦" || resultado2 == "♥") {
+    abajo.style.color = "red";
+  }
+  abajo.innerHTML = resultado2;
+
   console.log("Hello Rigo from the console!");
 };
